e5f91fb9d0b81f61e71b1c5eb73197f5a9d92d14
Davdi Yoon <hello@kamicrafted.com>
2019-12-17 20:12:25 -0500

    initializing project

---
+++ doczrc.js
@@ -0,0 +1,7 @@
+export default {
+  menu: [
+    'Welcome',
+    { name: 'Overview', menu: ['Intro to the Design System', 'How The Process Works', 'How to Contribute']}
+    { name: 'Components', menu: ['Alert', 'Button'] }
+  ]
+}
